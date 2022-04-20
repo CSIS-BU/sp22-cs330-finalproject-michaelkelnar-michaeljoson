339cba0bca9cff9db9fe6dc47afae42e40fa4ea2
MichaelJoson <mjoson@mail.bradley.edu>
2022-04-19 22:29:25 -0500

    1st commit

--- client_server/server-c.c
+++ client_server/server-c.c
@@ -5,11 +5,51 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <string.h>
 
 #define SERVER_PORT  5432
 #define MAX_PENDING  5
 #define MAX_LINE     256
 
+void response()
+{
+  
+  //opens txt file to read char from
+  FILE *fp;
+  char c;
+  
+  fp = fopen("a.txt", "r");
+  c = fgetc(fp);
+  
+  //TODO: next step is to change these if statements below to compare char to a word that the server chooses and printf the correct response
+  //compare char to a letter and printf a certain response
+  if (c == 'y')
+  {
+    printf("%s", "yes \n");
+  }
+  if (c == 'n')
+  {
+    printf("%s", "no \n");
+  }
+
+  //code to print out entire txt file (for testing purposes if needed)
+  /* 
+  while (c != EOF)
+  {
+    
+    //printf ("%c", c);
+    c = fgetc(fp);
+  }
+  */
+  
+
+  
+  fclose(fp);
+
+  
+
+  return 0;
+}
 
 int
 main()
@@ -18,6 +58,7 @@ main()
   char buf[MAX_LINE];
   int buf_len, addr_len;
   int s, new_s;
+  FILE* fp;
 
   /* build address data structure */
   bzero((char *)&sin, sizeof(sin));
@@ -44,9 +85,25 @@ main()
       perror("simplex-talk: accept");
       exit(1);
     }
+    
     while (buf_len = recv(new_s, buf, sizeof(buf), 0))
+    {
       //TODO: instead of just printing, use buffer to compare char to entire word then send back result to 
-      fputs(buf, stdout);
+      
+      fp = fopen("a.txt", "w");
+      fputs(buf, fp);
+      fclose(fp);
+      
+     response();
+      fflush(stdout);
+    }
+    //fp = fopen("a.txt","r");
+    //fflush(fp);
+    //fclose(fp);
+    //fflush(stdout);
     close(new_s);
+  
+  
   }
+
 }
